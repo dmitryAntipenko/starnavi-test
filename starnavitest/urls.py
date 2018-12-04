d7d4edfbb96281807fcbd1df4dc8b0add62f0391
Dmitry Antipenko <dmitrij.antipenko@gmail.com>
2018-12-04 02:21:53 +0200

    Fix csrftoken issue

--- starnavitest/urls.py
+++ starnavitest/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^api/', include('socnetapi.urls')),
     url(r'^auth/', include('rest_auth.urls')),
-    url(r'^registration/', include('rest_auth.registration.urls')),
+    url(r'^auth/registration/', include('rest_auth.registration.urls')),
 ]
